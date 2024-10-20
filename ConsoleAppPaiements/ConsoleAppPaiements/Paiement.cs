aa4e98460a99af6bfe5f863b03e6d7c61850402b
yanno237 <300133903@monboreal.ca>
2024-10-20 17:43:34 -0400

    nouvelle modification

---
+++ ConsoleAppPaiements/ConsoleAppPaiements/Paiement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiements
+{
+    internal class Paiement
+    {
+        public double Montant { get; set; }
+        public string Description { get; set; }
+
+        public Paiement(double montant , string description) 
+        {
+          Montant = montant;
+            Description = description;
+        }
+
+        public virtual void AfficherDetails()
+        {
+            Console.WriteLine($"le montant correspond a {Montant} , avec pour description  {Description}");
+        }
+    }
+}
