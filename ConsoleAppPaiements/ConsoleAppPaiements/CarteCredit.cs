c4d9a9343574b962fcb2ef956a97a466fb239670
yanno237 <300133903@monboreal.ca>
2024-10-20 18:12:52 -0400

    mes modifications

---
+++ ConsoleAppPaiements/ConsoleAppPaiements/CarteCredit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPaiements
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte { get; set; }
+
+        public CarteCredit  (double montant, string description ,int numeroCarte) : base (montant,description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($" le montant correspond a {Montant} , avec pour description  {Description},le numero de la carte est {NumeroCarte}"); 
+        }
+    }
+}
--- ConsoleAppPaiements/ConsoleAppPaiements/Program.cs
+++ ConsoleAppPaiements/ConsoleAppPaiements/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using ConsoleAppPaiements;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+
+        // je suis entrain de faire l ajout de 3 element 
+
+        CarteCredit one = new CarteCredit(1000,"mon achat today",123456);
+        CarteCredit two = new CarteCredit(1500, "mon achat de dimanche", 123897);
+        CarteCredit three = new CarteCredit(2400, "mon achat de la semaine prochaine ", 200456);
+
+        //affichage 
+
+        one.AfficherDetails();
+        two.AfficherDetails();
+        three.AfficherDetails();
+
     }
 }
